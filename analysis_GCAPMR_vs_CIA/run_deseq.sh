5d79fe6087ce9cd4e8d27d212bec6a227aad9f01
hurben <hurben@bioinfo@gmail.com>
2022-01-05 09:59:25 -0600

    update

--- analysis_GCAPMR_vs_CIA/run_deseq.sh
+++ analysis_GCAPMR_vs_CIA/run_deseq.sh
@@ -3,4 +3,4 @@ meta_file=aortitis_pmr_gca.meta.tsv
 output_file=aortitis_pmrgca.deg.tsv
 echo "Proceeding >" $data_file $meta_file $output_file
 
-Rscript ../../src/RNAseqPipeline_SungLab/deseq2.r $data_file $meta_file $output_file
+Rscript ../src/RNAseqPipeline_SungLab/deseq2.r $data_file $meta_file $output_file
